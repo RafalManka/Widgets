69e87a6a4a6371e251f305d68037d3b0072044ff
rafal.manka <rafal@manka.info>
2018-08-06 16:26:48 +0400

    first commit

---
+++ Widgets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Widgets
+//
+//  Created by rafal.manka on 06/08/2018.
+//  Copyright © 2018 Rafal Manka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
